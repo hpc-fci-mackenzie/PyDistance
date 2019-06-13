9245f6165f056886b49f9dd69630d7946b8920e8
Marcelo <mvrozanti@hotmail.com>
2019-06-13 20:18:30 -0300

    better python test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,20 @@
 import PyDistance as pd
 
-x = [1, 2, 3, 4,   5, 80]
-y = [7, 8, 9, 10, 11, 12]
+from scipy.spatial.distance import euclidean, cityblock as manhattan, cosine
 
-print("Calculate Sequence of:\nVALUE: ", x)
-result = pd.euclidean(x, y)
-print("RESULT: ", result)
+p = [1, 2, 3,  4,  5, 80]
+q = [7, 8, 9, 10, 11, 12]
+
+np_p = np.array(p);
+np_q = np.array(q);
+
+distance_matrix = {
+        pd.euclidean:euclidean,
+        pd.manhattan:manhattan,
+        pd.cosine:cosine,
+        }
+
+for pyd_f, scipy_f in distance_matrix.items():
+    res_pyd = pyd_f(p,q)
+    res_scipy = scipy_f(np_p,np_q)
+    assert(abs(res_pyd - res_scipy) < 0.00001)
